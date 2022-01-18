cdcdeb46e2d8f77a8b2bb770007ed1e12fd89f76
Marco Tsang <hkmarcoot@gmail.com>
2022-01-18 14:19:30 +0000

    add libs/data.js with week table and sampleInfo

---
+++ libs/data.js
@@ -0,0 +1,101 @@
+export const weeks_table = [
+  {
+    week_id: 1,
+    topic: "Foundations",
+  },
+  {
+    week_id: 2,
+    topic: "Javascript",
+  },
+  {
+    week_id: 3,
+    topic: "Front-end",
+  },
+  {
+    week_id: 4,
+    topic: "Back-end",
+  },
+  {
+    week_id: 5,
+    topic: "Databases",
+  },
+  {
+    week_id: 6,
+    topic: "Testing",
+  },
+  {
+    week_id: 7,
+    topic: "React Basics",
+  },
+  {
+    week_id: 8,
+    topic: "React Advanced",
+  },
+  {
+    week_id: 9,
+    topic: "Client Project",
+  },
+  {
+    week_id: 10,
+    topic: "Agile",
+  },
+  {
+    week_id: 11,
+    topic: "Computer Science",
+  },
+  {
+    week_id: 12,
+    topic: "Architecture",
+  },
+  {
+    week_id: 13,
+    topic: "Project",
+  },
+  {
+    week_id: 14,
+    topic: "Project",
+  },
+  {
+    week_id: 15,
+    topic: "Project",
+  },
+  {
+    week_id: 16,
+    topic: "Project",
+  },
+];
+
+export const sampleInfo = [
+  {
+    id: 1,
+    week: 1,
+    tags: "html",
+    summary: "learn to use h1 tag",
+    link: "www.samplelink01.com",
+    isComplete: true,
+  },
+  {
+    id: 2,
+    week: 1,
+    tags: "css",
+    summary: "learn basic css styling",
+    link: "www.samplelink02.com",
+    isComplete: false,
+  },
+  {
+    id: 3,
+    week: 2,
+    tags: "Javascript",
+    summary: "use Javascript to make an object moves",
+    link: "www.samplelink03.com",
+    isComplete: true,
+  },
+  {
+    id: 4,
+    week: 3,
+    tags: "front-end",
+    summary: "make a simple front-end today",
+    link: "www.samplelink04.com",
+    isComplete: false,
+  },
+];
