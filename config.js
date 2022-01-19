931572cdfa3a859e2d0e29f5051d766e5460224b
Marco Tsang <hkmarcoot@gmail.com>
2022-01-19 12:34:09 +0000

    connectionString

--- config.js
+++ config.js
@@ -1,12 +1,15 @@
 //---------------------------//
-export const db = {
-	user: process.env.USER,
-	host: process.env.DBHOST,
-	database: process.env.DBNAME,
-	password: process.env.DBPASSWORD,
-	port: process.env.DBPORT,
-	// dbhost
-	// dbname
-	// username
-	// password
-};
+// export const db = {
+// 	user: process.env.USER,
+// 	host: process.env.DBHOST,
+// 	database: process.env.DBNAME,
+// 	password: process.env.DBPASSWORD,
+// 	port: process.env.DBPORT,
+
+// 	// dbhost
+// 	// dbname
+// 	// username
+// 	// password
+// };
+
+export const connectionString = process.env.DATABASE_URL;
