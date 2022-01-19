166c2aeb54dc33d01c44291c7b2e9ca2cf34ee61
Marco Tsang <hkmarcoot@gmail.com>
2022-01-19 14:20:56 +0000

    add deleteNote in router

--- routes/weeks.js
+++ routes/weeks.js
@@ -2,47 +2,58 @@ import express from "express";
 const router = express.Router();
 
 import {
-	getAllWeeks,
-	getWeekNotes,
-	addANoteToTheWeek,
+  getAllWeeks,
+  getWeekNotes,
+  addANoteToTheWeek,
+  deleteANote,
 } from "../models/weeks.js";
 
 router.get("/", async (req, res) => {
-	//Example: localhost:3000/cats/
-	const allWeeks = await getAllWeeks();
+  //Example: localhost:3000/cats/
+  const allWeeks = await getAllWeeks();
 
-	res.json({ success: true, message: `all weeks`, payload: allWeeks });
+  res.json({ success: true, message: `all weeks`, payload: allWeeks });
 });
 
 // GET specific cat by id
 router.get("/:id", async (req, res) => {
-	const id = Number(req.params.id);
-	const requestedNotes = await getWeekNotes(id);
-	res.json({
-		success: true,
-		message: `Found Note ${id}`,
-		payload: requestedNotes,
-	});
+  const id = Number(req.params.id);
+  const requestedNotes = await getWeekNotes(id);
+  res.json({
+    success: true,
+    message: `Found Note ${id}`,
+    payload: requestedNotes,
+  });
 });
 
 router.post("/:id", async function (req, res) {
-	const week = Number(req.params.id);
-	const tags = req.body.tags;
-	const summary = req.body.summary;
-	const link = req.body.link;
-	const isComplete = req.body.isComplete;
-	const newNote = await addANoteToTheWeek(
-		week,
-		tags,
-		summary,
-		link,
-		isComplete
-	);
-	res.json({
-		success: true,
-		message: `new note created`,
-		payload: newNote,
-	});
+  const week = Number(req.params.id);
+  const tags = req.body.tags;
+  const summary = req.body.summary;
+  const link = req.body.link;
+  const isComplete = req.body.isComplete;
+  const newNote = await addANoteToTheWeek(
+    week,
+    tags,
+    summary,
+    link,
+    isComplete
+  );
+  res.json({
+    success: true,
+    message: `new note created`,
+    payload: newNote,
+  });
+});
+
+router.delete("/information/:id", async function (req, res) {
+  const id = Number(req.params.id);
+  const deleteNote = await deleteANote(id);
+  res.json({
+    success: true,
+    message: `deleted a note`,
+    payload: deleteNote,
+  });
 });
 
 export default router;
